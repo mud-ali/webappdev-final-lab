c91a0cca25c6ba615dd86afcf1b64ad781470e03
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-04-18 19:09:57 -0400

    change html file paths

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const port = 65352;
 app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
 
-app.get("/:page.:type", (req, res) => {
-    if (!["html","htm","txt"].includes(req.params.type)) {
-        res.send("404 File Not Found");
-        return;
-    }
-    res.sendFile(__dirname + `/views/${req.params.page}.${req.params.type}`);
+app.get("/:page", (req, res) => {
+    res.sendFile(__dirname + `/views/${req.params.page}.html`);
 });
 
 app.get("/", (req, res) => {
